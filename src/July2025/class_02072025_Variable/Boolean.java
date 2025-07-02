d16a6f74f2e703c980ab8013994b2a9d470e23c2
Mehul <mehulpagi005@outlook.com>
2025-07-02 21:46:35 +0530

    variable_Long_name

---
+++ src/July2025/class_02072025_Variable/Boolean.java
@@ -0,0 +1,12 @@
+package July2025.class_02072025_Variable;
+
+public class Boolean {
+    public static void main(String[] args) {
+        boolean Switch_on = true;
+        boolean have_audi =false;
+        boolean is_indian= true;
+        boolean is_today_sunday=false;
+
+
+    }
+}
