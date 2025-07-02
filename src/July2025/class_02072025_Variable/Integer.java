a319e4a07312f68117d8d4959ec69da0cae3a2f2
Mehul <mehulpagi005@outlook.com>
2025-07-02 22:01:58 +0530

    variable_Interger Number

---
+++ src/July2025/class_02072025_Variable/Integer.java
@@ -0,0 +1,13 @@
+package July2025.class_02072025_Variable;
+
+public class Integer {
+    public static void main(String[] args) {
+        //Integer can store values for -214,748,364,8 to 214,748,364,7
+
+        int mobil_number=978654321;
+        int reandom_number=987654321;
+        int negative_number=-987654321;
+
+
+    }
+}
