ba1cc98df26207d031dd22e1b2c375ad3ee6f4c7
Mehul <mehulpagi005@outlook.com>
2025-07-02 21:55:08 +0530

    variable_Byte

---
+++ src/July2025/class_02072025_Variable/Bytes.java
@@ -0,0 +1,22 @@
+package July2025.class_02072025_Variable;
+
+public class Bytes {
+    public static void main(String[] args) {
+        //byte can store min values from (-128) to 127
+        byte b =-127;
+        byte c= -128;
+        //byte d= -129;
+        byte e=-125;
+        byte f=0;
+        byte g=1;
+        byte h=10;
+        byte i=100;
+        byte j=110;
+        byte k=127;
+        //byte l=128;
+
+
+
+
+    }
+}
