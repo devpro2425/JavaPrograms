655cc77d002d066a2baebc2f24db132ef5b15f74
Mehul <mehulpagi005@outlook.com>
2025-07-02 22:24:57 +0530

    variable_Long

---
+++ src/July2025/class_02072025_Variable/Long.java
@@ -0,0 +1,9 @@
+package July2025.class_02072025_Variable;
+
+public class Long {
+    public static void main(String[] args) {
+        //Long contain -2^63 to 2^63-1
+        long a=922337201;
+
+    }
+}
