847069715a9632eb0a26e60ff70371db2f4de017
Mehul <mehulpagi005@outlook.com>
2025-07-02 22:32:15 +0530

    variable_Double

---
+++ src/July2025/class_02072025_Variable/Double.java
@@ -0,0 +1,9 @@
+package July2025.class_02072025_Variable;
+
+public class Double {
+    public static void main(String[] args) {
+        double D=1.7987987;
+        double F=98765465465.4654654;
+        double k=22123154165465465468548579876546546354654.65946546548748;
+    }
+}
