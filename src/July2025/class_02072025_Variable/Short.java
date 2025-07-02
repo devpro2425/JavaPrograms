6f666205da763351cfd1497d89567b4242fb3dd0
Mehul <mehulpagi005@outlook.com>
2025-07-02 21:58:25 +0530

    variable_Short

---
+++ src/July2025/class_02072025_Variable/Short.java
@@ -0,0 +1,16 @@
+package July2025.class_02072025_Variable;
+
+public class Short {
+    public static void main(String[] args) {
+        //short can contain (-32768) to 32767
+        short a=-32768;
+        short b=-32768;
+        //short c=-32769;
+        short d=0;
+        short e=32000;
+        short f=32767;
+        //short g=32769;
+
+
+    }
+}
