fc7a04d4c82d6126a45b4a60189f4d489712d6b0
Mehul <mehulpagi005@outlook.com>
2025-07-08 06:56:45 +0530

    Operators

---
+++ src/July2025/ex_08072025_Operators/AirthmeticOperators.java
@@ -0,0 +1,20 @@
+package July2025.ex_08072025_Operators;
+
+public class AirthmeticOperators {
+    public static void main(String[] args) {
+        int a=20;
+        int b=10;
+        /*
+        * + > addition
+        * - > substraction
+        * * multiplication
+        * / division
+        * % modulation
+        * */
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
---
+++ src/July2025/ex_08072025_Operators/AssignmentOperator.java
@@ -0,0 +1,11 @@
+package July2025.ex_08072025_Operators;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a);
+
+        //assignment operator can be define by = sign
+        //use to RHS values to the LHS = Left hand size
+    }
+}
