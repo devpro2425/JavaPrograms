26494a09c3fc19e65c339f8e4e2b608bc5039a11
Mehul <mehulpagi005@outlook.com>
2025-07-08 08:12:27 +0530

    Operators

---
+++ src/July2025/ex_08072025_Constant_Literal/Constant.java
@@ -0,0 +1,9 @@
+package July2025.ex_08072025_Constant_Literal;
+
+public class Constant {
+    public static void main(String[] args) {
+        //for constant final keyword is used
+        final float pi=3.14f;
+        System.out.println(pi);
+    }
+}
---
+++ src/July2025/ex_08072025_Operators/RelatioalOperators.java
@@ -0,0 +1,14 @@
+package July2025.ex_08072025_Operators;
+
+public class RelatioalOperators {
+    public static void main(String[] args) {
+        // < Less than
+        // <= less than or equal to
+        // > greater than
+        // >= greater than or equal to
+        // ! not equal to
+
+
+        System.out.println();
+    }
+}
