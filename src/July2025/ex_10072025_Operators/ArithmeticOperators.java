34b2d3786bee22a7dfdd14df7ac06aefa459b986
Mehul <mehulpagi005@outlook.com>
2025-07-10 09:43:49 +0530

    Operators

---
+++ src/July2025/ex_10072025_Operators/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+package July2025.ex_10072025_Operators;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        //+,-,*,/,%
+        int a=20;
+        int b=5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);//moudules
+    }
+}
---
+++ src/July2025/ex_10072025_Operators/Assignment_Operators.java
@@ -0,0 +1,12 @@
+package July2025.ex_10072025_Operators;
+
+public class Assignment_Operators {
+    public static void main(String[] args) {
+        int a=15;
+        int b=90;
+        int c=75;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
