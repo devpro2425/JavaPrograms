3ecc7dbdd2f3470f7ee1d43d10961c34be874b79
Mehul <mehulpagi005@outlook.com>
2025-07-10 09:30:52 +0530

    Literals

---
+++ src/July2025/ex_10072025_Literals/escape_character.java
@@ -0,0 +1,24 @@
+package July2025.ex_10072025_Literals;
+
+import javax.sound.midi.Soundbank;
+
+public class escape_character {
+    public static void main(String[] args) {
+        char c1='A';
+        char c2='B';
+        char c3='C';
+
+        char new_line='\n';
+        char tab_line='\t';
+        char back_space='\b';
+        char car_r='\r';
+
+        System.out.println("Mehul"+new_line+"pagi");
+        System.out.println(new_line);
+        System.out.println("Mehul"+tab_line+"Pagi");
+        System.out.println(new_line);
+        System.out.println("Mehul"+back_space+"Pagi");
+        System.out.println(new_line);
+        System.out.println("Mehul"+car_r+"Pagi");
+    }
+}
