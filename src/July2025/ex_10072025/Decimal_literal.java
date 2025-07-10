a6301cf5eb8cccd42838908b1203d320719f9e76
Mehul <mehulpagi005@outlook.com>
2025-07-10 08:01:15 +0530

    Literals

---
+++ src/July2025/ex_10072025/Decimal_literal.java
@@ -0,0 +1,16 @@
+package July2025.ex_10072025;
+
+public class Decimal_literal {
+    public static void main(String[] args) {
+        int age=78;
+        int market=75;
+        int negative=-50;
+        int zero=0;
+        System.out.println(age);
+        System.out.println(market);
+        System.out.println(negative);
+        System.out.println(zero);
+
+
+    }
+}
---
+++ src/July2025/ex_10072025/octal_literal.java
@@ -0,0 +1,9 @@
+package July2025.ex_10072025;
+
+public class octal_literal {
+    public static void main(String[] args) {
+        int octal=0101;
+        //octal values start wiht 0 ;
+        System.out.println(octal);
+    }
+}
