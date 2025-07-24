4fefcbb6ee15c4b303e348dc224f4d28a9a02ef5
Mehul <mehulpagi005@outlook.com>
2025-07-24 08:22:00 +0530

    Scanner

--- src/July2025/ex_18072025/ScannerInput.java
+++ src/July2025/ex_18072025/ScannerInput.java
@@ -9,14 +9,14 @@ public class ScannerInput {
         int Name= scanner.nextInt();
         System.out.println("Enter the Name\n");
         System.out.println("Your name is "+Name);
-
+        scanner.close();
         int age= scanner.nextInt();
         System.out.println("Enter the age\n");
-        System.out.println("Your ageis "+age);
+        System.out.println("Your age is "+age);
 
         int Salary= scanner.nextInt();
         System.out.println("Enter the Salary\n");
         System.out.println("Your salary "+Salary);
-        //System.out.println(age>25?"Allowed to vote":"Not allowed to vote");
+        //System.out.println(age>25?"Allowed to vote": "Not allowed to vote");
         scanner.close();
 }}
---
+++ src/July2025/ex_24072025_If_Condition/If_Condition.java
@@ -0,0 +1,18 @@
+package July2025.ex_24072025_If_Condition;
+
+import java.util.Scanner;
+
+public class If_Condition {
+    public static void main(String[] args) {
+        //  Scanner sc= new Scanner(System.in);
+        //  int age =sc.nextInt();
+        //int age=25;
+        //allowed to vote
+        //if the age is > 18
+        int age = 25;
+        if (age > 18) {
+            System.out.println("Allowed to vote");
+        }
+
+    }
+}
