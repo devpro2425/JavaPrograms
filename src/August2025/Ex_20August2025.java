6bbb05238ad431abbf44301e1852361e83b30619
Mehul <mehulpagi005@outlook.com>
2025-08-28 21:54:06 +0530

    Scanner

---
+++ src/August2025/Ex_20August2025.java
@@ -0,0 +1,7 @@
+package August2025;
+
+public class Ex_20August2025 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ src/August2025/String1.java
@@ -0,0 +1,9 @@
+package August2025;
+
+public class String1 {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println("Hello " + name);
+        System.out.println();
+    }
+}
